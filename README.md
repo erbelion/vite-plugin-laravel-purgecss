40d56c2b2469a0e123d3cc71b5d7eab006134a98
maciek <erbelionx@gmail.com>
2022-11-05 03:40:02 +0100

    templates option now includes "resources/views"

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # vite-plugin-laravel-purgecss
 
-[![Experimental version](https://img.shields.io/badge/-experimental-red)](#) [![Get package from npm](https://img.shields.io/npm/v/@erbelion/vite-plugin-laravel-purgecss?color=blue)](https://www.npmjs.com/package/@erbelion/vite-plugin-laravel-purgecss)
+[![Experimental version](https://img.shields.io/badge/-experimental-red)](#) [![Get package from npm](https://img.shields.io/npm/v/@erbelion/vite-plugin-laravel-purgecss?color=blue)](https://www.npmjs.com/package/@erbelion/vite-plugin-laravel-purgecss) [![Downloads](https://img.shields.io/npm/dt/@erbelion/vite-plugin-laravel-purgecss)](https://www.npmjs.com/package/@erbelion/vite-plugin-laravel-purgecss)
 
 A [Vite](https://github.com/vitejs/vite) plugin that integrates [PurgeCSS](https://github.com/FullHuman/purgecss) with [Laravel](https://github.com/laravel/laravel) template assets.
 
@@ -44,9 +44,9 @@ export default {
 | Parameter | Path(s) to be processed |
 | ----------- | ----------- |
 | blade | `resources/views/**/*.blade.php` |
-| svelte | `resources/js/**/*.svelte` |
-| vue | `resources/js/**/*.vue` |
-| react | `resources/js/**/*.tsx`<br/>`resources/js/**/*.ts`<br/>`resources/js/**/*.jsx`<br/>`resources/js/**/*.js`<br/>`resources/js/**/*.html` |
-| angular | `resources/js/**/*.html` |
+| svelte | `resources/views/**/*.svelte`<br/>`resources/js/**/*.svelte` |
+| vue | `resources/views/**/*.vue`<br/>`resources/js/**/*.vue` |
+| react | `resources/views/**/*.tsx`<br/>`resources/views/**/*.ts`<br/>`resources/views/**/*.jsx`<br/>`resources/views/**/*.js`<br/>`resources/views/**/*.html`<br/>`resources/js/**/*.tsx`<br/>`resources/js/**/*.ts`<br/>`resources/js/**/*.jsx`<br/>`resources/js/**/*.js`<br/>`resources/js/**/*.html` |
+| angular | `resources/views/**/*.html`<br/>`resources/js/**/*.html` |
 
 You may also provide custom paths via `paths` option.
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import PurgeCSS, { UserDefinedSafelist } from "purgecss";
+import PurgeCSS, { UserDefinedSafelist } from "purgecss"
 
 type Options = {
     templates?: string[],
@@ -15,17 +15,25 @@ export default (options?: Options) => {
             if (!cssFiles) return
             
             let paths = []
-            options?.paths?.forEach(key => { paths.push(key) })
-            options?.templates?.forEach(key => {
-                switch(key){
+            options?.paths?.forEach(path => { paths.push(path) })
+            options?.templates?.forEach(path => {
+                switch(path){
                     case 'blade':
-                        paths.push('resources/views/**/*.blade.php')
+                        paths.push(
+                            'resources/views/**/*.blade.php'
+                        )
                         break
                     case 'svelte':
-                        paths.push('resources/js/**/*.svelte')
+                        paths.push(
+                            'resources/js/**/*.svelte',
+                            'resources/views/**/*.svelte'
+                        )
                         break
                     case 'vue':
-                        paths.push('resources/js/**/*.vue')
+                        paths.push(
+                            'resources/js/**/*.vue',
+                            'resources/views/**/*.vue'
+                        )
                         break
                     case 'react':
                         paths.push(
@@ -33,11 +41,19 @@ export default (options?: Options) => {
                             'resources/js/**/*.ts',
                             'resources/js/**/*.jsx',
                             'resources/js/**/*.js',
-                            'resources/js/**/*.html'
+                            'resources/js/**/*.html',
+                            'resources/views/**/*.tsx',
+                            'resources/views/**/*.ts',
+                            'resources/views/**/*.jsx',
+                            'resources/views/**/*.js',
+                            'resources/views/**/*.html'
                         )
                         break
                     case 'angular':
-                        paths.push('resources/js/**/*.html')
+                        paths.push(
+                            'resources/js/**/*.html',
+                            'resources/views/**/*.html'
+                        )
                         break
                 }
             })
