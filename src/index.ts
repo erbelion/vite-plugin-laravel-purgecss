19290c1255f86e8d10f7531233dc58bf6542f719
maciek <erbelionx@gmail.com>
2023-06-16 00:08:23 +0200

    vite.config.ts compatibility, dependencies update
    compatible with laravel 10.13

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,44 @@
-import PurgeCSS, { UserDefinedSafelist } from 'purgecss'
-import getTemplatePath from './getTemplatePath'
+import { PurgeCSS, UserDefinedSafelist } from "purgecss"
+import getTemplatePath from "./getTemplatePath"
+import { Plugin } from "vite"
+import { OutputBundle, OutputAsset } from "vite/node_modules/rollup/dist/rollup"
 
 type Options = {
-    templates?: string[],
-    paths?: string[],
+    templates?: string[]
+    paths?: string[]
     safelist?: UserDefinedSafelist
 }
 
-export default (options?: Options) => {
+const isOutputAsset = (
+    bundle: OutputBundle[keyof OutputBundle]
+): bundle is OutputAsset => bundle.type === "asset"
+
+export default (options?: Options): Plugin => {
     return {
-        name: 'vite-plugin-laravel-purgecss',
-        enforce: 'post',
-        async generateBundle(_options, bundle) {
-            const cssFiles = Object.keys(bundle).filter(key => key.endsWith('.css'))
+        name: "vite-plugin-laravel-purgecss",
+        enforce: "post",
+        async generateBundle(_options, bundle: OutputBundle) {
+            const cssFiles = Object.keys(bundle).filter((key) =>
+                key.endsWith(".css")
+            )
             if (!cssFiles) return
-            
+
             let paths = []
-            options?.paths?.forEach(path => paths.push(path))
-            options?.templates?.forEach(template => paths.push(getTemplatePath(template)))
+            options?.paths?.forEach((path) => paths.push(path))
+            options?.templates?.forEach((template) =>
+                paths.push(getTemplatePath(template))
+            )
 
             for (const file of cssFiles) {
+                if (!isOutputAsset(bundle[file])) continue
+
                 const purged = await new PurgeCSS().purge({
                     content: paths,
-                    css: [{raw: bundle[file].source}],
-                    safelist: options?.safelist || []
+                    css: [{ raw: bundle[file]["source"] }],
+                    safelist: options?.safelist || [],
                 })
-                bundle[file].source = purged[0].css
+                bundle[file]["source"] = purged[0].css
             }
-        }
+        },
     }
 }
