c737de8d808f03bfe84bddbd0a88b04b78da0338
maciek <erbelionx@gmail.com>
2023-06-16 01:53:04 +0200

    Update getTemplatePath.ts

--- src/getTemplatePath.ts
+++ src/getTemplatePath.ts
@@ -1,16 +1,14 @@
-export default function(template: string): string {
-    switch(template){
-        case 'blade':
-            return 'resources/views/**/*.blade.php'
-        case 'svelte':
-            return 'resources/{js,views}/**/*.svelte'
-        case 'vue':
-            return 'resources/{js,views}/**/*.vue'
-        case 'react':
-            return 'resources/{js,views}/**/*.{tsx,ts,jsx,js,html}'
-        case 'angular':
-            return 'resources/{js,views}/**/*.html'
-        default:
-            throw new Error(`Template ${template} is not supported`)
-    }
+const templates = {
+    blade: "resources/views/**/*.blade.php",
+    svelte: "resources/{js,views}/**/*.svelte",
+    vue: "resources/{js,views}/**/*.vue",
+    react: "resources/{js,views}/**/*.{tsx,ts,jsx,js,html}",
+    angular: "resources/{js,views}/**/*.html",
+}
+
+export default (template: string): string => {
+    if (!(template in templates))
+        throw new Error(`Template ${template} is not supported`)
+
+    return templates[template]
 }
